1319fbcd716120733b6e32239b230c9adb2be5eb
bernadette privateer <lynprivateer@gmail.com>
2017-08-07 14:54:39 -0400

    some styling is finished

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-let container =document.querySelector("container");
+
+let container =document.getElementById('container');
+
+
+let name = "Bernadette Privateer";
+let bio = "Bio";
+
+container.innerHTML += `<h1>${name}</h1>`
 
 function reqListener(){
   let data = JSON.parse(this.responseText);
 
-container.innerHTML += <li>${data.name}</li>
-container.innerHTML += <li>${data.html_url}</li>
-container.innerHTML += <li>${data.email}</li>
-container.innerHTML += <li>${data.company}</li>
-container.innerHTML += <li>${data.}</li>
+
+
+container.innerHTML += `<li>${data.name}</li>`;
+container.innerHTML += `<li><a href="${data.html_url}">${data.html_url}</a></li>`;
+container.innerHTML += `<li>${data.email}</li>`;
+container.innerHTML += `<li>${data.company}</li>`;
+container.innerHTML += `<li><a href="${data.blog}">${data.blog}</a></li>`;
+
+
+container.innerHTML +=`<h3>${bio}</h3> <p>${data.bio} <span><img src="${data.avatar_url}"></span></p>`;
+
+// container.innerHTML += `<span><img src="${data.avatar_url}"></span>`;
 
   console.log(data);
   console.log(data.name);
 }
 
-let req = new XMLHttpRequest;
+let req = new XMLHttpRequest();
 req.open ("GET", "https://api.github.com/users/bprivateer");
 req.addEventListener("load", reqListener);
 req.send();
